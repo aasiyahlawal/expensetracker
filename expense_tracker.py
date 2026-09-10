a3680bc6ac8cbe521ee326259f7a01bbfc8a858e
aasiyahlawal <aasiyahlawal1@gmail.com>
2026-09-10 15:35:27 +0100

    Database connection
    - Code for database connection established

--- expense_tracker.py
+++ expense_tracker.py
@@ -10,11 +10,12 @@ import sqlite3
 #Return to menu
 #Exit
 
-conn = sqlite3.connect(expenses) #not yet created expenses database
-conn.close()
+connection = sqlite3.connect("expenses.db") #not yet created expenses database
+connection.close()
 
 try:
-    with sqlite3.connect(expenses.db) as conn:
-        pass
+    with sqlite3.connect("expenses.db") as conn:
+        print("Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+        #pass
 except sqlite3.OperationalError as error:
     print("Failed to open a database", error)    
